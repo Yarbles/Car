ccec2c9513097176820e02875f04ec6eef617c3c
Yarbles <reidbaldwin@gmail.com>
2015-03-02 10:32:45 -0800

    Create Car.php

---
+++ Car.php
@@ -0,0 +1,59 @@
+<?php
+
+class Car
+{
+    public $make_model;
+    public $price;
+    public $miles;
+}
+
+
+  $porsche = new Car();
+   $porsche->make_model = "2014 Porsche 911";
+   $porsche->price = 114991;
+   $porsche->miles = 7864;
+
+  $ford = new Car();
+   $ford->make_model = "2011 Ford F450";
+   $ford->price = 55995;
+   $ford->miles = 14241;
+
+  $lexus = new Car();
+   $lexus->make_model = "2013 Lexus RX 350";
+   $lexus->price = 44700;
+   $lexus->miles = 20000;
+
+  $mercedes = new Car();
+   $mercedes->make_model = "Mercedes Benz CLS550";
+   $mercedes->price = 39900;
+   $mercedes->miles = 37979;
+
+ $cars = array($porsche, $ford, $lexus, $mercedes);
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <title>Car Dealership!</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Deals</h1>
+        <ul>
+            <?php
+                foreach ($cars as $car) {
+                    echo "<li>";
+                    echo $car->make_model;
+                    echo "<ul>";
+                    echo "<li> $car->price </li>";
+                    echo "<li> $car->miles </li>";
+                    echo "</ul>";
+                    echo "</li>";
+                }
+            ?>
+        </ul>
+    </div>
+</body>
+ </html>
